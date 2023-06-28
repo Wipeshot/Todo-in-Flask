ebcf00c26447957af5ca734861b00d18aa03cc3d
BuildTools <79708519+Wipeshot@users.noreply.github.com>
2023-06-28 17:03:48 +0200

    Email now not Nullable

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ class LoginForm(FlaskForm):
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False)
-    email = db.Column(db.String(50), nullable=True)
+    email = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
 
 
